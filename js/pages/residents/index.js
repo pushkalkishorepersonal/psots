b675566d5ec9a21012623785a128b100407dc773
Pushkal Kishore <pushkalk@tekion.com>
2026-04-13 03:57:06 +0530

    Fix shared component imports

--- js/pages/residents/index.js
+++ js/pages/residents/index.js
@@ -6,8 +6,8 @@
 
 import { session }           from '../../core/auth.js';
 import { logger }            from '../../core/logger.js';
-import { Toast }             from '../../components/shared/Toast.js';
-import { Steps }             from '../../components/shared/Steps.js';
+import Toast             from '../../components/shared/Toast.js';
+import Steps             from '../../components/shared/Steps.js';
 import FlatSelector      from '../../components/resident/FlatSelector.js';
 import { residentService }   from '../../services/resident.service.js';
 import { rateLimitService }  from '../../services/rateLimit.service.js';
